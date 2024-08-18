7ec47aad872fb3657a361c303fb730d2b448f1e1
Kingwell24 <3118497634@qq.com>
2024-08-18 20:58:34 +0800

    update

---
+++ test_database/csv_to_sql.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# 读取 CSV 文件
+df = pd.read_csv(r"C:\Users\31184\Desktop\微软课题\convert\owid-co2-data.csv")
+
+# 创建数据库连接
+engine = create_engine('mysql+mysqlconnector://root:52zz468275@localhost/CO2')
+
+# 将数据导入到 MySQL 表中
+df.to_sql('people', con=engine, if_exists='append', index=False)
